e832cbcc1fb9a26606479ec61bb276db7855f108
sufiyan11919 <sufiyanshaikh11919@gmail.com>
2025-05-04 14:24:35 -0700

    terraform fmt

--- terraform/uat/main.tf
+++ terraform/uat/main.tf
@@ -14,8 +14,8 @@ provider "aws" {
 }
 
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
-  version = "20.0.0"
+  source       = "terraform-aws-modules/eks/aws"
+  version      = "20.0.0"
   cluster_name = "weather-${terraform.workspace}"
   vpc_id       = var.vpc_id
   subnet_ids   = var.subnet_ids
