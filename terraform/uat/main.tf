f2042cb0f43a9a83096fa9f42a5a681c8f1b073c
sufiyan11919 <sufiyanshaikh11919@gmail.com>
2025-05-04 16:42:00 -0700

    testing

--- terraform/uat/main.tf
+++ terraform/uat/main.tf
@@ -15,8 +15,6 @@ module "eks" {
 
   cluster_name    = "weather-${terraform.workspace}"
   cluster_version = "1.29"
-  region          = var.region
-
   vpc_id     = var.vpc_id
   subnet_ids = var.subnet_ids
 
